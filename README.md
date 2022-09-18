bd17bf66fc7cb1892668db66f72764976ae697c2
Fisher2911 <fisher1205l@gmail.com>
2022-09-18 19:42:16 -0400

    Added README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+MCA Stuff:
+
+https://github.com/Querz/mcaselector
+https://discord.gg/h942U8U
--- src/main/java/io/github/fisher2911/jarrunner/JarRunner.java
+++ src/main/java/io/github/fisher2911/jarrunner/JarRunner.java
@@ -13,21 +13,15 @@ public final class JarRunner extends JavaPlugin {
         // Plugin startup logic
         try {
             this.saveDefaultConfig();
+            if (this.getConfig().getBoolean("ran", false)) return;
             final String[] args = this.formatArgs(this.getConfig().getString("args", ""));
-            this.getLogger().info("Running jar with args: " + args);
             final String jarName = this.getConfig().getString("jar-name", "");
-//            this.getLogger().info("Running jar with name: " + jarName);
-//            this.getLogger().info("First path: " + this.getDataFolder().toPath());
-//            this.getLogger().info("First path: " + this.getDataFolder().toPath().getParent());
-//            this.getLogger().info("First path: " + this.getDataFolder().toPath().getParent().getParent());
             final Path path = Path.of(jarName);
-            this.getLogger().info("Running jar with path: " + path);
             JarClassLoader loader = new JarClassLoader(path.toUri().toURL());
-            this.getLogger().info("Created jar loader");
             String main = loader.getMainClassName();
-            this.getLogger().info("Found main class: " + main);
+            this.getConfig().set("ran", true);
+            this.saveConfig();
             loader.invokeClass(main, args);
-            this.getLogger().info("Invoked jar");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -36,25 +30,25 @@ public final class JarRunner extends JavaPlugin {
     private String[] formatArgs(String currentArgs) {
         boolean hasQuote = false;
         final List<String> args = new ArrayList<>();
-         StringBuilder sb = new StringBuilder();
-         for (char c : currentArgs.toCharArray()) {
-             if (c == ' ') {
-                 if (hasQuote) {
-                     sb.append(c);
-                 } else {
-                     args.add(sb.toString());
-                     sb = new StringBuilder();
-                 }
-                 continue;
-             }
-             if (c == '\"') {
-                 hasQuote = !hasQuote;
-                 continue;
-             }
-             sb.append(c);
-         }
-         args.add(sb.toString());
-         return args.toArray(new String[0]);
+        StringBuilder sb = new StringBuilder();
+        for (char c : currentArgs.toCharArray()) {
+            if (c == ' ') {
+                if (hasQuote) {
+                    sb.append(c);
+                } else {
+                    args.add(sb.toString());
+                    sb = new StringBuilder();
+                }
+                continue;
+            }
+            if (c == '\"') {
+                hasQuote = !hasQuote;
+                continue;
+            }
+            sb.append(c);
+        }
+        args.add(sb.toString());
+        return args.toArray(new String[0]);
     }
 
     @Override
